f9c2db8b1ef8f842b42cfeb7ea36a0803c90b97d
dougalcaleb <dougalcaleb@gmail.com>
2025-12-23 23:39:01 -0700

    Finish removing HelloWorld

--- src/tte-server-manager/src/App.vue
+++ src/tte-server-manager/src/App.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 import DesktopNav from './components/shared/DesktopNav.vue';
 import MobileNav from './components/shared/MobileNav.vue';
 import StickyHeader from './components/shared/StickyHeader.vue';
@@ -17,7 +16,6 @@ import screen from './mixins/screen';
 export default {
 	mixins: [ screen ],
 	components: {
-		HelloWorld,
 		DesktopNav,
 		StickyHeader,
 		MobileNav,
