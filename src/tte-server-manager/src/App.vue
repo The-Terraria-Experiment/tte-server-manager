89ac325c6cce1e091f35f42598d8d489cef4496a
dougalcaleb <dougalcaleb@gmail.com>
2025-12-24 16:52:14 -0700

    Get Desktop view working properly

--- src/tte-server-manager/src/App.vue
+++ src/tte-server-manager/src/App.vue
@@ -1,9 +1,19 @@
 <template>
 	<div>
 		<StickyHeader />
-		<MobileNav v-if="isMobile" />
-		<DesktopNav v-else />
-		<RouterView />
+
+		<div v-if="isMobile">
+			<div class="p-4">
+				<RouterView />
+			</div>
+			<MobileNav />
+		</div>
+		<div v-else class="flex w-full h-max">
+			<DesktopNav />
+			<div class="ml-22 p-8">
+				<RouterView />	
+			</div>
+		</div>
 	</div>
 </template>
 
