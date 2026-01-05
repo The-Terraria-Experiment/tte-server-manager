cf707b658f6b2e46a32e42c2b5dd608592f480cb
dougalcaleb <dougalcaleb@gmail.com>
2026-01-04 20:48:05 -0700

    Dropdown component

---
+++ src/tte-server-manager/src/components/common/Dropdown.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="relative inline-block w-full font-main font-bold">
+		<select 
+			class="appearance-none pr-9 w-full bg-gray-5 text-white-0 rounded-lg px-4 py-2 outline-none cursor-pointer font-main font-bold"
+			@change="emitInput"
+			:disabled="disabled"
+		>
+			<template v-for="option of options">
+				<option 
+					:class="['font-main font-semibold', optionClass]"
+					:value="option.id"
+				>
+					{{ option.text }}
+				</option>
+			</template>
+		</select>
+		<Icon 
+			icon="caret-down" 
+			size="6" 
+			color="text-teal-4" 
+			class="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 h-6 w-6 text-white"
+		/>
+	</div>
+</template>
+
+<script>
+import Icon from './Icon.vue';
+
+
+export default {
+	mixins: [],
+	components: {
+		Icon,
+	},
+	props: {
+		modelValue: {
+			default: null
+		},
+		options: {
+			type: Array,
+			default: []
+		},
+		optionClass: {
+			type: String,
+			default: ""
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	},
+	data() {
+		return {
+			
+		}
+	},
+	computed: {
+		
+	},
+	methods: {
+		emitInput(event) {
+			if (this.disabled) return;
+
+			this.$emit('input', event.currentTarget.value);
+		}
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
