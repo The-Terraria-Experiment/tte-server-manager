27b78ce6888904c97e17be631b94bfa4aeaab7e4
dougalcaleb <dougalcaleb@gmail.com>
2026-01-12 14:29:48 -0700

    Add global functions for checking resource permissions

--- src/tte-server-manager/src/plugins/permissions.js
+++ src/tte-server-manager/src/plugins/permissions.js
@@ -1,14 +1,24 @@
 import { createValidatePermissions, createCheckPermissions } from '../util/createValidatePermissions'
+import { createValidateResourcePermissions, createCheckResourcePermissions } from '../util/createValidateResourcePermissions'
 
 export default {
 	install(app) {
 		const validate = createValidatePermissions();
 		const check = createCheckPermissions();
 
+		const validateResource = createValidateResourcePermissions();
+		const checkResource = createCheckResourcePermissions();
+
 		app.config.globalProperties.$validatePermissions = validate;
 		app.provide('validatePermissions', validate);
 
 		app.config.globalProperties.$checkPermissions = check;
 		app.provide('checkPermissions', check);
+
+		app.config.globalProperties.$validateResourcePermissions = validateResource;
+		app.provide('validateResourcePermissions', validateResource);
+
+		app.config.globalProperties.$checkResourcePermissions = checkResource;
+		app.provide('checkResourcePermissions', checkResource);
 	},
 };
