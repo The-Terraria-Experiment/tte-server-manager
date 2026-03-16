a7503590b2857e9e342fc9241f1ab73fb4f71e28
Caleb <dougalcaleb@gmail.com>
2026-03-15 20:58:05 -0600

    Ban Management

---
+++ src/tte-server-manager/src/components/pages/tools/server/ServerConfig.vue
@@ -0,0 +1,53 @@
+<template>
+	<!-- <div class="flex flex-col sm:grid sm:grid-cols-2 gap-2"> -->
+		<TshockConfigFile 
+			:selected-server-data="selectedServerData"
+			:selected-instance="selectedInstance" 
+		/>
+
+		<ManageBans 
+			v-if="selectedInstance && selectedServerData.state"
+			:selected-server-data="selectedServerData"
+			:selected-instance="selectedInstance" 
+		/>
+	<!-- </div> -->
+</template>
+
+<script>
+import ManageBans from './ManageBans.vue';
+import TshockConfigFile from './TshockConfigFile.vue';
+
+
+export default {
+	mixins: [],
+	components: {
+		ManageBans,
+		TshockConfigFile,
+	},
+	props: {
+		selectedServerData: {
+			type: Object,
+			required: true
+		},
+		selectedInstance: {
+			type: [String, null],
+			required: true
+		},
+	},
+	data() {
+		return {
+			
+		}
+	},
+	computed: {
+		
+	},
+	methods: {
+		
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
