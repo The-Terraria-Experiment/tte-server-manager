34dab231b63eade6f54d38d8362018e000a52a8e
Caleb <dougalcaleb@gmail.com>
2026-04-19 21:49:23 -0600

    Update packages and config to prepare for ts replacement, converted first file

---
+++ src/lambda/shared/vars.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" />
+
+export const PERM_TABLE = process.env.ACTIVE_ENV === "prod" ? "ttesm-user-perms" : "ttesm-user-perms-stage";
+export const SYSTEM_TABLE = process.env.ACTIVE_ENV === "prod" ? "ttesm-system" : "ttesm-system-stage";
