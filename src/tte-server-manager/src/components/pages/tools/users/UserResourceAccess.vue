bc46367447c31c1344e9275ffc18bc41b0aa042f
Caleb Dougal <35942679+dougalcaleb@users.noreply.github.com>
2026-05-16 22:36:53 -0600

    Fix resource access character key clipping over mobile nav

--- src/tte-server-manager/src/components/pages/tools/users/UserResourceAccess.vue
+++ src/tte-server-manager/src/components/pages/tools/users/UserResourceAccess.vue
@@ -7,75 +7,73 @@
 			<p class="text-gray-6 ml-2 text-lg">User Resource Permissions</p>
 		</template>
 		<template #content>
-			<FuzzyMatchSearch 
-				class="ml-4 mb-4"
-				placeholder="Filter users..."
-				:data="sortedPermissionsData"
-				comparisonKey="displayName"
-				@update="filteredUserData = $event"
-				sortResults
-			/>
-
-			<div class="relative z-20 ml-4 text-gray-9 font-main font-bold mb-14 sm:mb-0">
-				<p class="text-gray-7">Character Key:</p>
-				<p>
-					<span class="font-mono font-bold">i:</span> instance
-				</p>
-				<p>
-					<span class="font-mono font-bold">p:</span> file path
-				</p>
-			</div>
-
-			<div v-if="loading.instances" class="flex items-center m-4">
-				<Spinner class="h-4 w-4 text-gray-8" thickness="4" />
-				<p class="font-main font-bold text-gray-8 ml-2">Loading...</p>
-			</div>
-			<div v-else class="grid overflow-x-auto pt-40 relative pr-40 text-sm max-w-max -mt-20" :style="userPermsCols" @scroll="updateUserTableScroll">
-				<div :class="['sticky left-0 bg-gray-3 px-4 py-2 flex items-center z-10', stickyShadow]">
-					<div class="bg-gray-3 h-60 absolute w-full left-0 bottom-5 z-10 facadeStickyShadow"></div>
-					<p class="font-main font-bold text-cream relative z-20">USER</p>
+			<div class="relative z-0">
+				<FuzzyMatchSearch
+					class="ml-4 mb-4"
+					placeholder="Filter users..."
+					:data="sortedPermissionsData"
+					comparisonKey="displayName"
+					@update="filteredUserData = $event"
+					sortResults
+				/>
+				<div class="relative z-20 ml-4 text-gray-9 font-main font-bold mb-14 sm:mb-0">
+					<p class="text-gray-7">Character Key:</p>
+					<p>
+						<span class="font-mono font-bold">i:</span> instance
+					</p>
+					<p>
+						<span class="font-mono font-bold">p:</span> file path
+					</p>
 				</div>
-				<template v-for="permValue in allPerms">
-					<div class="px-2 flex items-center w-20 relative">
-						<p class="font-mono text-cream origin-left -rotate-45 absolute left-8 bottom-0 text-nowrap">
-							<span class="text-gray-7">{{ permValue.prefix }}</span> 
-							<span>{{ permValue.displayText }}</span>
-						</p>
-					</div>
-				</template>
-
-				<template v-for="(user, idx) of filteredUserData">
-					<div :class="['sticky left-0 p-2 flex items-center z-10 overflow-x-auto', stickyShadow, idx%2 ? 'bg-gray-3' : 'bg-gray-4']">
-						<p class="font-mono font-semibold text-cream text-nowrap">{{ user.displayName || user.username }}</p>
+				<div v-if="loading.instances" class="flex items-center m-4">
+					<Spinner class="h-4 w-4 text-gray-8" thickness="4" />
+					<p class="font-main font-bold text-gray-8 ml-2">Loading...</p>
+				</div>
+				<div v-else class="grid overflow-x-auto pt-40 relative pr-40 text-sm max-w-max -mt-20" :style="userPermsCols" @scroll="updateUserTableScroll">
+					<div :class="['sticky left-0 bg-gray-3 px-4 py-2 flex items-center z-10', stickyShadow]">
+						<div class="bg-gray-3 h-60 absolute w-full left-0 bottom-5 z-10 facadeStickyShadow"></div>
+						<p class="font-main font-bold text-cream relative z-20">USER</p>
 					</div>
 					<template v-for="permValue in allPerms">
-						<div :class="['px-2 flex items-center w-20 relative justify-center border-r-2 border-gray-2', idx%2 ? 'bg-gray-3' : 'bg-gray-4']">
-							<Checkbox 
-								class="h-5 w-5"
-								:disabled="!$checkPermissions(PERMISSIONS.users.permissions.write)"
-								:value="hasAccess(user, permValue)"
-								@input="setUserPerm(user.userID, permValue, $event)"
-							/>
+						<div class="px-2 flex items-center w-20 relative">
+							<p class="font-mono text-cream origin-left -rotate-45 absolute left-8 bottom-0 text-nowrap">
+								<span class="text-gray-7">{{ permValue.prefix }}</span>
+								<span>{{ permValue.displayText }}</span>
+							</p>
 						</div>
 					</template>
-				</template>
-			</div>
-
-			<div 
-				class="w-full flex justify-end p-4"
-				v-show="dirtyPermissions"
-			>
-				<div v-if="!loading.save" class="flex">
-					<FlexButton :variant="BTN_VARIANT.DANGER" class="ml-4" @click="discardPermChanges">
-						<p class="font-main font-bold py-2 px-8 md:px-12 text-sm">DISCARD</p>
-					</FlexButton>
-					<FlexButton :variant="BTN_VARIANT.PRIMARY" class="ml-4" @click="savePermChanges">
-						<p class="font-main font-bold py-2 px-8 md:px-12 text-sm">SAVE</p>
-					</FlexButton>
+					<template v-for="(user, idx) of filteredUserData">
+						<div :class="['sticky left-0 p-2 flex items-center z-10 overflow-x-auto', stickyShadow, idx%2 ? 'bg-gray-3' : 'bg-gray-4']">
+							<p class="font-mono font-semibold text-cream text-nowrap">{{ user.displayName || user.username }}</p>
+						</div>
+						<template v-for="permValue in allPerms">
+							<div :class="['px-2 flex items-center w-20 relative justify-center border-r-2 border-gray-2', idx%2 ? 'bg-gray-3' : 'bg-gray-4']">
+								<Checkbox
+									class="h-5 w-5"
+									:disabled="!$checkPermissions(PERMISSIONS.users.permissions.write)"
+									:value="hasAccess(user, permValue)"
+									@input="setUserPerm(user.userID, permValue, $event)"
+								/>
+							</div>
+						</template>
+					</template>
 				</div>
-				<div v-else class="flex items-center pr-4">
-					<Spinner class="h-4 w-4 text-teal-3" thickness="4" />
-					<p class="font-main font-bold text-teal-3 ml-2">Saving...</p>
+				<div
+					class="w-full flex justify-end p-4"
+					v-show="dirtyPermissions"
+				>
+					<div v-if="!loading.save" class="flex">
+						<FlexButton :variant="BTN_VARIANT.DANGER" class="ml-4" @click="discardPermChanges">
+							<p class="font-main font-bold py-2 px-8 md:px-12 text-sm">DISCARD</p>
+						</FlexButton>
+						<FlexButton :variant="BTN_VARIANT.PRIMARY" class="ml-4" @click="savePermChanges">
+							<p class="font-main font-bold py-2 px-8 md:px-12 text-sm">SAVE</p>
+						</FlexButton>
+					</div>
+					<div v-else class="flex items-center pr-4">
+						<Spinner class="h-4 w-4 text-teal-3" thickness="4" />
+						<p class="font-main font-bold text-teal-3 ml-2">Saving...</p>
+					</div>
 				</div>
 			</div>
 		</template>
