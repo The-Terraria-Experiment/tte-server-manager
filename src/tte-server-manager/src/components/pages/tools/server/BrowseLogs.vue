dbbe3f91b62a5eccda0c3d73ea35d3a195b0509e
Caleb Dougal <35942679+dougalcaleb@users.noreply.github.com>
2026-05-24 21:21:43 -0600

    Logs browser

--- src/tte-server-manager/src/components/pages/tools/server/BrowseLogs.vue
+++ src/tte-server-manager/src/components/pages/tools/server/BrowseLogs.vue
@@ -1,146 +1,163 @@
 <template>
-	<StatusTile 
-		class="grow mt-2 gradient-tile"
-		collapsible
-		:perm-required="PERMISSIONS.server.logs.read"
-		:loading="loading"
-	>
-		<template #header>
-			<Icon icon="file-lines" color="text-gray-6" size="4" />
-			<p class="text-gray-6 ml-2 text-lg">Player Logs</p>
-		</template>
-		<template #summary>
-			<p class="text-2xl text-teal-4">Logs available</p>
-		</template>
-		<template #content>
-			<div class="px-4 pb-4">
-				<div class="bg-gray-2 p-4 rounded-md">
-					<p class="font-main font-bold text-gray-7">FILTER LOGS:</p>
-					<div class="flex gap-2 items-center mt-2">
-						<Dropdown
-							class="sm:max-w-1/4"
-							inputClass="bg-teal-3 text-white-1"
-							iconColor="text-white-1"
-							:options="filterByOptions"
-							v-model="logsFilter"
-						/>
-						
-						<div class="py-1 pl-3 pr-1 bg-gray-3 rounded-md font-main font-bold text-white-0 flex items-center">
-							<p>From:</p>
-							<div 
-								@click="timeFilterStartPopupOpen = true"
-								class="bg-blue-0 hover:bg-blue-1 transition-colors duration-100 rounded px-2 py-1 ml-2 text-white-1 cursor-pointer flex items-center"
-							>
-								<p class="mr-2">{{ timeFilterStart || 'forever ago' }}</p> 
-								<Icon icon="edit" color="text-white-1" size="5" />
+	<div>
+		<StatusTile
+			class="grow mt-2 gradient-tile"
+			collapsible
+			:perm-required="PERMISSIONS.server.logs.read"
+			:loading="loading"
+		>
+			<template #header>
+				<Icon icon="file-lines" color="text-gray-6" size="4" />
+				<p class="text-gray-6 ml-2 text-lg">Player Logs</p>
+			</template>
+			<template #summary>
+				<p class="text-2xl text-teal-4">Logs available</p>
+			</template>
+			<template #content>
+				<div class="px-4 pb-4">
+					<div class="bg-gray-2 p-4 rounded-md">
+						<p class="font-main font-bold text-gray-7">FILTER LOGS:</p>
+						<div class="flex flex-wrap gap-2 items-center mt-2">
+							<Dropdown
+								class="sm:max-w-1/4"
+								inputClass="bg-teal-3 text-white-1"
+								iconColor="text-white-1"
+								:options="filterByOptions"
+								v-model="logsFilter"
+							/>
+		
+							<div class="py-1 pl-3 pr-1 bg-gray-3 rounded-md font-main font-bold text-white-0 flex items-center">
+								<p>From:</p>
+								<div
+									@click="timeFilterStartPopupOpen = true"
+									class="bg-blue-0 hover:bg-blue-1 transition-colors duration-100 rounded px-2 py-1 ml-2 text-white-1 cursor-pointer flex items-center"
+								>
+									<p class="mr-2">{{ timeFilterStart || 'forever ago' }}</p>
+									<Icon icon="edit" color="text-white-1" size="5" />
+								</div>
 							</div>
+							<div class="py-1 pl-3 pr-1 bg-gray-3 rounded-md font-main font-bold text-white-0 flex items-center">
+								<p>To:</p>
+								<div
+									@click="timeFilterEndPopupOpen = true"
+									class="bg-blue-0 hover:bg-blue-1 transition-colors duration-100 rounded px-2 py-1 ml-2 text-white-1 cursor-pointer flex items-center"
+								>
+									<p class="mr-2">{{ timeFilterEnd || 'now' }}</p>
+									<Icon icon="edit" color="text-white-1" size="5" />
+								</div>
+							</div>
+							<DateTimePickerPopup
+								:open="timeFilterStartPopupOpen"
+								@close="timeFilterStartPopupOpen = false"
+								@cancel="timeFilterStartPopupOpen = false; timeFilterStart = null;"
+								v-model="timeFilterStart"
+							/>
+							<DateTimePickerPopup
+								:open="timeFilterEndPopupOpen"
+								@close="timeFilterEndPopupOpen = false"
+								@cancel="timeFilterEndPopupOpen = false; timeFilterEnd = null;"
+								v-model="timeFilterEnd"
+							/>
 						</div>
-
-						<div class="py-1 pl-3 pr-1 bg-gray-3 rounded-md font-main font-bold text-white-0 flex items-center">
-							<p>To:</p>
-							<div 
-								@click="timeFilterEndPopupOpen = true"
-								class="bg-blue-0 hover:bg-blue-1 transition-colors duration-100 rounded px-2 py-1 ml-2 text-white-1 cursor-pointer flex items-center"
-							>
-								<p class="mr-2">{{ timeFilterEnd || 'now' }}</p> 
-								<Icon icon="edit" color="text-white-1" size="5" />
-							</div>
+						<div v-if="logsFilter === FILTER.EVENT" class="flex flex-wrap gap-2 mt-2">
+							<template v-for="(eventName, eventCode) in EVENT_NAMES">
+								<div
+									class="flex items-center bg-blue-0 px-2 py-1 rounded-md cursor-pointer hover:bg-blue-1"
+									@click="eventFilter.has(eventCode) ? eventFilter.delete(eventCode) : eventFilter.add(eventCode)"
+								>
+									<Checkbox
+										class="h-4 w-4"
+										:value="eventFilter.has(eventCode)"
+									/>
+									<span class="ml-2 font-main font-bold text-white-1">
+										{{ eventName }}
+									</span>
+								</div>
+							</template>
 						</div>
-
-						<DateTimePickerPopup 
-							:open="timeFilterStartPopupOpen"
-							@close="timeFilterStartPopupOpen = false"
-							@cancel="timeFilterStartPopupOpen = false; timeFilterStart = null;"
-							v-model="timeFilterStart" 
-						/>
-						<DateTimePickerPopup 
-							:open="timeFilterEndPopupOpen"
-							@close="timeFilterEndPopupOpen = false"
-							@cancel="timeFilterEndPopupOpen = false; timeFilterEnd = null;"
-							v-model="timeFilterEnd" 
-						/>
+						<div v-if="logsFilter === FILTER.PLAYER" class="mt-2">
+							<ValueInput v-model="playerFilter" placeholder="Player username" />
+						</div>
+						<FlexButton
+							class="mt-4"
+							:variant="BTN_VARIANT.SECONDARY"
+							leftIcon="cloud-download"
+							leftIconSize="5"
+							:disabled="loading || sameQuery"
+							@input="fetchInitial"
+						>
+							RUN QUERY
+						</FlexButton>
 					</div>
-
-					<div v-if="logsFilter === FILTER.EVENT" class="flex gap-2 mt-2">
-						<template v-for="(eventName, eventCode) in EVENT_NAMES">
-							<div 
-								class="flex items-center bg-blue-0 px-2 py-1 rounded-md cursor-pointer hover:bg-blue-1" 
-								@click="eventFilter.has(eventCode) ? eventFilter.delete(eventCode) : eventFilter.add(eventCode)"
-							>
-								<Checkbox
-									class="h-4 w-4"
-									:value="eventFilter.has(eventCode)"
-								/>
-								<span class="ml-2 font-main font-bold text-white-1">
-									{{ eventName }}
-								</span>
-							</div>
-						</template>
+					<div v-if="queryWasRun" class="mt-4 mx-2">
+						<p class="font-main font-bold text-gray-8">{{ logs.length }} log entries loaded</p>
+						<FlexButton
+							v-if="logs.length > 0"
+							class="mt-2"
+							:variant="BTN_VARIANT.SECONDARY"
+							leftIcon="external"
+							leftIconSize="4"
+							@input="logViewPopupOpen = true"
+						>
+							VIEW LOGS
+						</FlexButton>
 					</div>
-
-					<div v-if="logsFilter === FILTER.PLAYER" class="mt-2">
-						<ValueInput v-model="playerFilter" placeholder="Player username" />
-					</div>
-
-					<FlexButton
-						class="mt-4"
-						:variant="BTN_VARIANT.SECONDARY"
-						leftIcon="cloud-download"
-						leftIconSize="5"
-						:disabled="loading || sameQuery"
-						@input="fetchInitial"
-					>
-						RUN QUERY
-					</FlexButton>
 				</div>
+			</template>
+		</StatusTile>
 
-				<div v-if="queryWasRun">
-					<p class="mt-4 text-gray-6 font-bold italic">Dates and times are displayed in your time zone</p>
-					<div class="grid log-grid py-2 text-white-0 font-mono text-sm overflow-x-auto">
-						<div class="text-white-0 p-1 text-md font-bold bg-teal-1">Timestamp</div>
-						<div class="text-white-0 p-1 text-md font-bold bg-teal-1">Username</div>
-						<div class="text-white-0 p-1 text-md font-bold bg-teal-1">Event</div>
-						<div class="text-white-0 p-1 text-md font-bold bg-teal-1">World</div>
-						<div class="text-white-0 p-1 text-md font-bold bg-teal-1">Player Group</div>
-						<div class="text-white-0 p-1 text-md font-bold bg-teal-1">TShock Login</div>
-						<div class="text-white-0 p-1 text-md font-bold bg-teal-1">Players Online</div>
-						<template v-for="(log, i) in currentLogsPage">
-							<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ new Date(log.timestamp).toLocaleString() }}</div>
-							<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.playerName }}</div>
-							<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.eventType }}</div>
-							<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.worldName }}</div>
-							<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.playerGroup }}</div>
-							<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.isLoggedIn }}</div>
-							<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.playersActive }}</div>
-						</template>
-					</div>
-					<div class="flex items-center">
-						<FlexButton
-							class="px-2!"
-							:variant="BTN_VARIANT.SECONDARY"
-							leftIcon="chevron-left"
-							leftIconSize="5"
-							:disabled="logsPage === 0"
-							@input="previousPage"
-						/>
-						<p class="font-main font-bold mx-2 text-white-0">
-							Showing {{ logsPage * logPageSize }} to {{ Math.min((logsPage + 1) * logPageSize, logs.length) }}
-							of {{ logs.length }} fetched,
-							{{ lastFetchedLog ? '' : 'no' }} more available
-						</p>
-						<FlexButton
-							class="px-2!"
-							:variant="BTN_VARIANT.SECONDARY"
-							leftIcon="chevron-right"
-							leftIconSize="5"
-							:disabled="((logsPage + 1) * logPageSize) > logs.length && !lastFetchedLog"
-							@input="nextPage"
-						/>
-					</div>
+		<Popup
+			headerText="VIEW LOGS"
+			bodyClass="h-11/12 w-11/12"
+			:open="logViewPopupOpen"
+			@xClicked="logViewPopupOpen = false"
+		>
+			<div class="px-4 pb-4 flex flex-col h-full">
+				<p class="my-2 text-gray-6 font-bold italic">Dates and times are displayed in your time zone</p>
+				<div class="grid log-grid text-white-0 font-mono text-sm overflow-auto relative">
+					<div class="text-white-0 p-1 text-md font-bold bg-teal-1 sticky top-0">Timestamp</div>
+					<div class="text-white-0 p-1 text-md font-bold bg-teal-1 sticky top-0">Username</div>
+					<div class="text-white-0 p-1 text-md font-bold bg-teal-1 sticky top-0">Event</div>
+					<div class="text-white-0 p-1 text-md font-bold bg-teal-1 sticky top-0">World</div>
+					<div class="text-white-0 p-1 text-md font-bold bg-teal-1 sticky top-0">Player Group</div>
+					<div class="text-white-0 p-1 text-md font-bold bg-teal-1 sticky top-0">TShock Login</div>
+					<div class="text-white-0 p-1 text-md font-bold bg-teal-1 sticky top-0">Players Online</div>
+					<template v-for="(log, i) in currentLogsPage">
+						<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ new Date(log.timestamp).toLocaleString() }}</div>
+						<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.playerName }}</div>
+						<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.eventType }}</div>
+						<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.worldName }}</div>
+						<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.playerGroup }}</div>
+						<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.isLoggedIn }}</div>
+						<div :class="[{'bg-gray-4': i%2}, 'p-1']">{{ log.playersActive }}</div>
+					</template>
+				</div>
+				<div class="flex items-center mt-4">
+					<FlexButton
+						class="px-2!"
+						:variant="BTN_VARIANT.SECONDARY"
+						leftIcon="chevron-left"
+						leftIconSize="5"
+						:disabled="loading || logsPage === 0"
+						@input="previousPage"
+					/>
+					<p class="font-main font-bold mx-2 text-white-0 text-sm sm:text-md">
+						Showing {{ logsPage * logPageSize }} to {{ Math.min((logsPage + 1) * logPageSize, logs.length) }}
+						of {{ logs.length }} loaded entries
+						({{ lastFetchedLog ? '' : 'no ' }}more available)
+					</p>
+					<FlexButton
+						class="px-2!"
+						:variant="BTN_VARIANT.SECONDARY"
+						leftIcon="chevron-right"
+						leftIconSize="5"
+						:disabled="loading || (((logsPage + 1) * logPageSize) >= logs.length && !lastFetchedLog)"
+						@input="nextPage"
+					/>
 				</div>
 			</div>
-		</template>
-	</StatusTile>
+		</Popup>
+	</div>
 </template>
 
 <script>
@@ -149,6 +166,7 @@ import DateTimePickerPopup from '@/components/common/DateTimePickerPopup.vue';
 import Dropdown from '@/components/common/Dropdown.vue';
 import FlexButton from '@/components/common/FlexButton.vue';
 import Icon from '@/components/common/Icon.vue';
+import Popup from '@/components/common/Popup.vue';
 import ValueInput from '@/components/common/ValueInput.vue';
 import { useServerStore } from '@/stores/serverStore';
 import { post } from '@/util/api';
@@ -190,6 +208,7 @@ export default {
 		Checkbox,
 		ValueInput,
 		FlexButton,
+		Popup,
 	},
 	props: {
 		
@@ -215,6 +234,7 @@ export default {
 			playerFilter: "",
 			timeFilterStartPopupOpen: false,
 			timeFilterEndPopupOpen: false,
+			logViewPopupOpen: false,
 			loading: false,
 
 			logPageSize: 50,
@@ -240,7 +260,7 @@ export default {
 
 			if (this.logsFilter === FILTER.EVENT) {
 				if (this.eventFilter.size === 1) {
-					params.eventType = this.eventFilter.values()[0];
+					params.eventType = Array.from(this.eventFilter.values())[0];
 				} else {
 					params.eventTypes = Array.from(this.eventFilter);
 				}
@@ -265,7 +285,10 @@ export default {
 
 			this.logs = [];
 			this.lastFetchedLog = null;
+			this.logsPage = 0;
 			const success = await this.fetchLogs();
+			this.queryWasRun = true;
+			this.logViewPopupOpen = true;
 
 			if (success) {
 				this.lastQuery = this.queryParams;
@@ -294,7 +317,7 @@ export default {
 			try {
 				const result = await post(`/logging/${this.selectedInstance}/fetch`, PERMISSIONS.server.logs.read, this.queryParams);
 				this.logs.push(...result.entries);
-				this.lastFetchedLog = result.lastKey;
+				this.lastFetchedLog = result.lastValue;
 				return true;
 			} catch (e) {
 				this.$alert.error("Failed to fetch logs");
@@ -309,7 +332,7 @@ export default {
 			this.logsPage = this.logsPage - 1;
 		},
 		async nextPage() {
-			if ((this.logsPage + 1) * this.logPageSize > this.logs.length) {
+			if ((this.logsPage + 1) * this.logPageSize >= this.logs.length) {
 				if (this.lastFetchedLog) {
 					const success = await this.fetchLogs();
 					if (!success) {
