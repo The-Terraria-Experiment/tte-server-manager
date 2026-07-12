ebdc316e44fdc99c02f0c28aa6fbb3010ce5ecb7
Caleb Dougal <35942679+dougalcaleb@users.noreply.github.com>
2026-07-12 16:06:38 -0600

    Change checkbox accent color

--- src/tte-server-manager/src/components/common/Checkbox.vue
+++ src/tte-server-manager/src/components/common/Checkbox.vue
@@ -2,7 +2,7 @@
 	<input
 		ref="checkbox"
 		type="checkbox"
-		class="accent-teal-5"
+		class="accent-blue-3"
 		:class="{ 'cursor-pointer': !disabled, 'cursor-not-allowed': disabled }"
 		:checked="isChecked"
 		:disabled="disabled"
